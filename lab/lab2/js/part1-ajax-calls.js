124fbdb6ec9382c4ad1d5eb7c02bf71f7a9e4b54
jc020207 <54868777+jc020207@users.noreply.github.com>
2020-02-13 13:27:30 -0500

    week4

--- lab/lab2/js/part1-ajax-calls.js
+++ lab/lab2/js/part1-ajax-calls.js
@@ -9,3 +9,19 @@ var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/ton
   maxZoom: 20,
   ext: 'png'
 }).addTo(map);
+
+//bike = $.ajax('https://raw.githubusercontent.com/MUSA611-CPLN692-spring2020/datasets/master/json/philadelphia-bike-crashes-snippet.json')
+//bike.done(function(x){theData=x})
+//JSON.parse(theData)
+//parseData = JSON.parse(theData)
+
+//runs paralle as following code -- not on sequence -- that's why it works on console but not here
+var parsed;
+$.ajax('https://raw.githubusercontent.com/MUSA611-CPLN692-spring2020/datasets/master/json/philadelphia-bike-crashes-snippet.json')
+.done(function(resonse){parsed=JSON.parse(resonse)})
+
+var longlat = _.map(parsed,function(obj){
+  return {'lat':obj.lat_final,'lng':obj.long_final}
+})
+
+_.map(longlat,function(x){L.marker([x.lat,x.lng]).addTo(map)})
