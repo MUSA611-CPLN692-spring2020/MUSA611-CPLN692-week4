7b33a4f227eaefa8948a72ae2fb4038dddee8585
Yichen-Lei <54689123+Yichen-Lei@users.noreply.github.com>
2020-02-14 14:09:25 -0500

    Week 4 Labs
    Yichen Lei

--- lab/lab2/js/part1-ajax-calls.js
+++ lab/lab2/js/part1-ajax-calls.js
@@ -9,3 +9,5 @@ var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/ton
   maxZoom: 20,
   ext: 'png'
 }).addTo(map);
+
+$.ajax('https://raw.githubusercontent.com/MUSA611-CPLN692-spring2020/datasets/master/json/philadelphia-crime-snippet.json').done(function(x){console.log(x)});
